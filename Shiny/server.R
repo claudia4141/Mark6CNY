2591d0cd8d3893bd24d486465e426200ca48d388
fungyip <fung.yip@gmail.com>
2017-01-29 13:15:48 +0800

    Delete

--- Shiny/server.R
+++
@@ -1,7 +0,0 @@
-library(shiny)
-server <- function(input, output, session) {
-  observeEvent(input$do, {
-    session$sendCustomMessage(type = 'testmessage',
-                              message = '祝大家恭喜發財,新春快樂,財源滾滾來滾滾來')
-  })
-}
